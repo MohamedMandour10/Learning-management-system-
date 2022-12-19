ac744bec53f4b38caed11194666cf36aacb681fb
Mohammed Elsayed <115044826+MohamedMandour10@users.noreply.github.com>
2022-12-19 21:47:46 +0200

    update initial values with correct types

--- User.h
+++ User.h
@@ -14,12 +14,12 @@ protected:
 public:
    User(string &fn, string &ln ,string &m,string &e,int &a):firstName(fn),lastName(ln),mobile(m),email(e),age(a) {}
    
-   virtual string getFirstName()=0;
-   virtual string getLastName()=0;
-   virtual string getFullName() = 0;
+   virtual string getFirstName()=Null;
+   virtual string getLastName()=Null;
+   virtual string getFullName() = Null;
    virtual int getAge()=0;
-   virtual string getMobile()=0;
-   virtual string getMail()=0;
+   virtual string getMobile()=Null;
+   virtual string getMail()=Null;
 
 };
 
