ee25c3b0cb2a74f618b1319db83a601df4579d9f
omarnasser0 <100535160+omarnasser0@users.noreply.github.com>
2022-12-19 17:02:55 +0200

    Update User.h

--- User.h
+++ User.h
@@ -1,32 +1,27 @@
-//
-// Created by ahmed on 12/16/2022.
-//
-
-#ifndef BB_USER_H
-#define BB_USER_H
+#ifndef USER_H
+#define USER_H
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-class User
-{
+class User{
+
 protected:
-    string firstName, lastName, mobile, email; // why did we use char* instead of  using a string
-int age;
+    string firstName, lastName, mobile, email;
+    int age;
+
 public:
-   User(string &fn, string &ln ,string &m,string &e,int a):firstName(fn),lastName(ln),mobile(m),email(e),age(a)
-    {}
-    /*
-user():firstName("UNKNOWN"),lastName("UNKNOWN"),mobile("###########"),email("UNKNOWN@gmail.com"),age(18)
-{}*/
-virtual string getFirstName()=0;
-virtual string getLastName()=0;
-virtual int getAge()=0;
-virtual string getMobile()=0;
-virtual string getMail()=0;
-
-
+   User(string &fn, string &ln ,string &m,string &e,int &a):firstName(fn),lastName(ln),mobile(m),email(e),age(a) {}
+   
+   virtual string getFirstName()=0;
+   virtual string getLastName()=0;
+   virtual string getFullName() = 0;
+   virtual int getAge()=0;
+   virtual string getMobile()=0;
+   virtual string getMail()=0;
 
 };
 
 
-#endif //BB_USER_H
+#endif
